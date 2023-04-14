878bec7fb5ba743027863d3c62470166849a0a1a
Immmanuel Haag <9929800+immae1@users.noreply.github.com>
2023-04-14 11:35:38 +0200

    improved docs of package

--- adder.go
+++ adder.go
@@ -1,6 +1,9 @@
+// Package adder implements helpers for adding numbers. This will be used as Description of the package ....
 package adder
 
 // Add add two ints.
+//
+// extended description
 func Add(a, b int) int {
 
 	return a + b
